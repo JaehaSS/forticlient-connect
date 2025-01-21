1519e9a15c5cec1ba6d7f76adeee50fb2d44022c
JaehaSS <tkdgkwogk@gmail.com>
2025-01-21 17:20:18 +0900

    Shell Script Modify

--- vpn_profile.sh
+++ vpn_profile.sh
@@ -1,13 +1,17 @@
+#!/bin/bash
+
+
+
 #!/usr/bin/expect -f 
 spawn /opt/forticlient/fortivpn edit vpn
 expect -re "Type"
 send -- "1\r"
 set timeout -1
 expect -re "Remote Gateway:"
-send -- "$VPN_IP \r"
+send -- "$1 \r"
 set timeout -1
 expect -re "Port"
-send -- "$VPN_PORT \r"
+send -- "$2 \r"
 set timeout -1
 expect -re "Authentication"
 send -- "3\r"
