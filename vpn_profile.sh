486a868a53547f7c1c6db14355904383e1be5067
JaehaSS <tkdgkwogk@gmail.com>
2025-01-22 10:07:49 +0900

    Sciprt Test - 8

--- vpn_profile.sh
+++ vpn_profile.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/expect -f 
+echo "Hello! $env(vpn_ip)"
 spawn /opt/forticlient/fortivpn edit vpn
 expect -re "Type"
 send -- "1\r"
@@ -15,3 +16,4 @@ set timeout -1
 expect -re "Certificate"
 send -- "3\r"
 set timeout -1
+expect eof
