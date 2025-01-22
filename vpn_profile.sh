bd47000572050238fd756e5aeb7253bb3aab0dd6
JaehaSS <tkdgkwogk@gmail.com>
2025-01-22 10:45:56 +0900

    Sciprt Test - 11

--- vpn_profile.sh
+++ vpn_profile.sh
@@ -1,2 +1,18 @@
 #!/usr/bin/expect -f 
-spawn echo "Hello $env(vpn_ip) , Hello! $env(vpn_port)"
+spawn /opt/forticlient/fortivpn edit vpn
+expect -re "Type"
+send -- "1\r"
+set timeout -1
+expect -re "Remote Gateway:"
+send -- "$env(vpn_ip)\r"
+set timeout -1
+expect -re "Port"
+send -- "$env(vpn_port)\r"
+set timeout -1
+expect -re "Authentication"
+send -- "3\r"
+set timeout -1
+expect -re "Certificate"
+send -- "3\r"
+set timeout -1
+expect eof
