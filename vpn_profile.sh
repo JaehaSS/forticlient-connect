10bced8b0aab33aa607b833dbee9815775812102
JaehaSS <tkdgkwogk@gmail.com>
2025-01-22 09:24:20 +0900

    Sciprt Test - 3

--- vpn_profile.sh
+++ vpn_profile.sh
@@ -1,18 +1,24 @@
-#!/usr/bin/expect -f 
-echo "Test!! $1"
-spawn /opt/forticlient/fortivpn edit vpn
-expect -re "Type"
-send -- "1\r"
-set timeout -1
-expect -re "Remote Gateway:"
-send -- "$1 \r"
-set timeout -1
-expect -re "Port"
-send -- "$2 \r"
-set timeout -1
-expect -re "Authentication"
-send -- "3\r"
-set timeout -1
-expect -re "Certificate"
-send -- "3\r"
-set timeout -1
+# #!/usr/bin/expect -f 
+# spawn /opt/forticlient/fortivpn edit vpn
+# expect -re "Type"
+# send -- "1\r"
+# set timeout -1
+# expect -re "Remote Gateway:"
+# send -- "$vpn_ip \r"
+# set timeout -1
+# expect -re "Port"
+# send -- "$vpn_port \r"
+# set timeout -1
+# expect -re "Authentication"
+# send -- "3\r"
+# set timeout -1
+# expect -re "Certificate"
+# send -- "3\r"
+# set timeout -1
+
+
+#!/bin/bash
+
+
+echo "Hello!! $vpn_ip"
+echo "Vpn_port : $vpn_port"
